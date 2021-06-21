a21a33b9b1d7d29c3c4f22c3a659d280e0638855
lucie-hulin <43643526+lucie-hulin@users.noreply.github.com>
2021-06-21 18:47:24 +0200

    Update search.js
    javascript function

--- search.js
+++ search.js
@@ -2,3 +2,9 @@ function afficher(){
 var saisie =document.getElementById("texte1").value;
 document.getElementById("texte2").value=saisie ;
 }
+
+function loadInsta(string nom) : Observable<any>{
+      return this.http.get<any>("https://www.instagram.com/web/search/topsearch/?context=blended&query="+ nom);
+  console.log("https://www.instagram.com/web/search/topsearch/?context=blended&query="+ nom);
+}
+
