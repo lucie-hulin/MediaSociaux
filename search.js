5f375d1a17069cdf8f042703cfe992d7b8b448ad
lucie-hulin <43643526+lucie-hulin@users.noreply.github.com>
2021-06-27 12:39:36 +0200

    Update search.js

--- search.js
+++ search.js
@@ -2,10 +2,7 @@ function connecter(){
     localStorage.token = document.getElementById("token").value;
 console.log(token);
     window.location.pathname="/MediasSociaux/accueil.html";
-    /*loadInstaCaption(token);
-    loadInstaUsername(token);
-    loadInstaMedia(token);
-    loadInstatemps(token);*/
+   
 }
 
 
